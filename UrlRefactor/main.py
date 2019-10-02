6289f5aa59338ea3e01b54d9f26c1c547fe30f8c
RAINSAT <zyxwy1206@163.com>
2019-10-02 15:48:36 +0800

    update comment

--- UrlRefactor/main.py
+++ UrlRefactor/main.py
@@ -1,7 +1,8 @@
 # coding:utf-8
 
-import requests
 import re
+
+import requests
 
 
 class UrlRefactor(object):
@@ -9,11 +10,21 @@ class UrlRefactor(object):
         self.page_source = None
 
     def get_html(self, url, encoding='utf-8'):
+        """
+        获得网页源码
+        :param url: 网页url
+        :param encoding: 编码
+        :return: none
+        """
         response = requests.get(url)
         response.encoding = encoding
         self.page_source = response.text
 
     def get_navigator(self):
+        """
+        获得网页内所有 ul>li 并将内容返回
+        :return: dict
+        """
         if  self.page_source is not None:
             ul_content = re.findall("<ul(.+?)</ul>",self.page_source, re.S)
             content = dict()
